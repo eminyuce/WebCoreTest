c62dcbde01d79313b6c17c2bb5331813c368bb48
EMIN YUCE <eminyuce@gmail.com>
2018-05-01 11:27:37 -0400

    Table parsing

--- WebCore.UnitTest/UnitTest1.cs
+++ WebCore.UnitTest/UnitTest1.cs
@@ -49,63 +49,7 @@ namespace WebCore.UnitTest
             //GetAllTables(con);
         }
 
-        public void GetAllTables(String connectionString)
-        {
-            SqlConnection con =
-                          new SqlConnection(connectionString);
-
-            try
-            {
-
-                con.Open();
-
-
-                DataTable tblDatabases =
-                                con.GetSchema(
-                                           SqlClientMetaDataCollectionNames.Tables);
-
-
-                var list = new List<TableMetaData>();
-                foreach (DataRow rowDatabase in tblDatabases.Rows)
-                {
-                    var i = new TableMetaData();
-                    i.TableCatalog = rowDatabase["table_catalog"].ToString();
-                    i.TableSchema = rowDatabase["table_schema"].ToString();
-                    i.TableName = rowDatabase["table_name"].ToString();
-                    i.TableType = rowDatabase["table_type"].ToString();
-                    list.Add(i);
-                }
-
-                con.Close();
-
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
-        public class TableMetaData
-        {
-            public string TableCatalog { get; set; }
-            public string TableName { get; set; }
-            public string TableType { get; set; }
-            public string TableSchema { get; set; }
-
-            public string DatabaseTableName
-            {
-                get
-                {
-                    return String.Format("{0}.{1}.{2}", TableCatalog, TableSchema, TableName);
-                }
-            }
-            public string TableNameWithSchema
-            {
-                get
-                {
-                    return String.Format("{0}.{1}", TableSchema, TableName);
-                }
-            }
-        }
+       
 
     }
 }
---
+++ WebCoreTest.Domain/Helpers/DataTableHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCoreTest.Domain.Helpers
+{
+    public class DataTableHelper
+    {
+        public static object GetValue(DataRow row, string column)
+        {
+            return row.Table.Columns.Contains(column) ? row[column] : null;
+        }
+        public static string GetPrimaryKeys(DataTable myTable)
+        {
+            // Create the array for the columns.
+            DataColumn[] colArr;
+            colArr = myTable.PrimaryKey;
+            // Get the number of elements in the array.
+            //Response.Write("Column Count: " + colArr.Length + "<br>");
+            string primaryKey = "";
+            for (int i = 0; i < colArr.Length; i++)
+            {
+                //Response.Write(colArr[i].ColumnName + "<br>" + colArr[i].DataType + "<br>");
+                primaryKey = colArr[i].ColumnName;
+            }
+            return primaryKey;
+        }
+
+        public static String ToPrintConsole(DataTable dataTable)
+        {
+            var sb = new StringBuilder();
+            // Print top line
+            Console.WriteLine(new string('-', 75));
+            sb.AppendLine(new string('-', 1500));
+            // Print col headers
+            var colHeaders = dataTable.Columns.Cast<DataColumn>().Select(arg => arg.ColumnName);
+            foreach (String s in colHeaders)
+            {
+                Console.Write("| {0,-20}", s);
+                sb.Append(String.Format("| {0,-80}", s));
+            }
+            Console.WriteLine();
+            sb.AppendLine();
+            // Print line below col headers
+            Console.WriteLine(new string('-', 75));
+            sb.AppendLine(new string('-', 1500));
+            // Print rows
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (Object o in row.ItemArray)
+                {
+                    Console.Write("| {0,-20}", o.ToString());
+                    sb.Append(String.Format("| {0,-80}", o.ToString()));
+                }
+                Console.WriteLine();
+                sb.AppendLine("");
+            }
+
+            // Print bottom line
+            Console.WriteLine(new string('-', 75));
+            sb.AppendLine(new string('-', 1500));
+
+            return sb.ToString();
+        }
+    }
+}

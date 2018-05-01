1034dabf92229cd822e6e488adc735ccda0c16a6
EMIN YUCE <eminyuce@gmail.com>
2018-05-01 14:01:22 -0400

    MySql Support

--- WebCore.UnitMSTest/UnitTest1.cs
+++ WebCore.UnitMSTest/UnitTest1.cs
@@ -12,13 +12,30 @@ namespace WebCore.UnitMSTest
     [TestClass]
     public class UnitTest1
     {
+
+        private static IConfigurationRoot GetConfig()
+        {
+            return new ConfigurationBuilder()
+                        .SetBasePath(AppContext.BaseDirectory)
+                        .AddJsonFile("appsettings.Development.json")
+                        .Build();
+        }
+
+        [TestMethod]
+        public void TestMethod21()
+        {
+            IConfigurationRoot configuration = GetConfig();
+            MySqlRepository pp = new MySqlRepository();
+            //db_kodyazan
+            var con = configuration.GetConnectionString("MyDefaultConnection");
+            Console.WriteLine(con);
+            var items = pp.GetNwmHaberlers(con);
+            Console.WriteLine(items.Count);
+        }
         [TestMethod]
         public void TestMethod1()
         {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-.SetBasePath(AppContext.BaseDirectory)
-.AddJsonFile("appsettings.Development.json")
-.Build();
+            IConfigurationRoot configuration = GetConfig();
 
             var repo = new TableRepository();
 
@@ -27,6 +44,6 @@ namespace WebCore.UnitMSTest
             repo.GetSelectedTableMetaData(configuration.GetConnectionString("DefaultConnection"), "TestEY.dbo.Products");
         }
 
-      
+
     }
 }
---
+++ WebCoreTest.Domain/DB/Repositories/MySqlRepository.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using WebCoreTest.Domain.Helpers;
+
+namespace WebCoreTest.Domain.DB.Repositories
+{
+    public class MySqlRepository
+    {
+        public List<NwmHaberler> GetNwmHaberlers(string connectionString)
+        {
+            var list = new List<NwmHaberler>();
+            String commandText = @"SELECT * FROM db_kodyazan.haberler ORDER BY id DESC";
+            var parameterList = new List<MySqlParameter>();
+            DataSet dataSet = MySqlHelper.ExecuteDataset(connectionString, commandText, parameterList.ToArray());
+            if (dataSet.Tables.Count > 0)
+            {
+                list = dataSet.Tables[0].ToList<NwmHaberler>();
+            }
+            return list;
+        }
+        public class NwmHaberler
+        {
+            public int id { get; set; }
+            public int sira { get; set; }
+            public DateTime tarih { get; set; }
+            public int durum { get; set; }
+            public string seo { get; set; }
+            public string link { get; set; }
+            public string baslik_tr { get; set; }
+            public string keywords_tr { get; set; }
+            public string ozet_tr { get; set; }
+            public string detay_tr { get; set; }
+            public string baslik_en { get; set; }
+            public string keywords_en { get; set; }
+            public string ozet_en { get; set; }
+            public string detay_en { get; set; }
+            public DateTime haberTarihi { get; set; }
+            public string baslik_de { get; set; }
+            public string keywords_de { get; set; }
+            public string ozet_de { get; set; }
+            public string detay_de { get; set; }
+            public int tip { get; set; }
+            public string baslik_ar { get; set; }
+            public string keywords_ar { get; set; }
+            public string ozet_ar { get; set; }
+            public string detay_ar { get; set; }
+            public string yorum_tr { get; set; }
+            public string yorum_en { get; set; }
+        }
+    }
+}
